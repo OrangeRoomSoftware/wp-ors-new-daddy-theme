69741a7f4f47cbdb77d6665b60fa6551e8599548
Phill Kenoyer <phill@ors.bz>
2014-03-30 11:18:57 -0700

    Fix: removed class from post_classes.

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
     <?php if (have_posts()) : ?>
       <div class="<?php echo $col_width ?>">
         <?php while (have_posts()) : the_post(); ?>
-          <article <?php post_class('col_lg_12'); ?> id="post-<?php the_ID(); ?>">
+          <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
             <header>
               <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
               <time datetime="<?php the_time('Y-m-d')?>"><?php the_time('F jS, Y') ?></time>
